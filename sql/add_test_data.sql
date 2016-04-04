a9073c098b1e0acd4109733bf6037e1066249902
ValtteriV <valtteri.vistiaho@gmail.com>
2016-04-04 22:23:53 +0300

    Viikko 2 palautuksia

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,50 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Lisuke (nimi) VALUES ('Kinkku');
+INSERT INTO Lisuke (nimi) VALUES ('Ananas');
+INSERT INTO Lisuke (nimi) VALUES ('Tomaatti');
+INSERT INTO Lisuke (nimi) VALUES ('Feta');
+INSERT INTO Lisuke (nimi) VALUES ('Oliivi');
+INSERT INTO Lisuke (nimi) VALUES ('Sipuli');
+INSERT INTO Lisuke (nimi) VALUES ('Simpukka');
+INSERT INTO Lisuke (nimi) VALUES ('Tonnikala');
+INSERT INTO Lisuke (nimi) VALUES ('Aurajuusto');
+INSERT INTO Lisuke (nimi) VALUES ('Juusto');
+INSERT INTO Lisuke (nimi) VALUES ('Kana');
+INSERT INTO Lisuke (nimi) VALUES ('Salami');
+INSERT INTO Lisuke (nimi) VALUES ('Jauheliha');
+INSERT INTO Lisuke (nimi) VALUES ('Katkarapu');
+INSERT INTO Lisuke (nimi) VALUES ('Vihreä Pepperoni');
+INSERT INTO Lisuke (nimi) VALUES ('Kebabliha');
+INSERT INTO Lisuke (nimi) VALUES ('Herkkusieni');
+INSERT INTO Lisuke (nimi) VALUES ('Valkosipuli');
+INSERT INTO Lisuke (nimi) VALUES ('Pepperoni');
+INSERT INTO Lisuke (nimi) VALUES ('Jalopeno');
+INSERT INTO Lisuke (nimi) VALUES ('Paprika');
+INSERT INTO Lisuke (nimi) VALUES ('Smetana');
+
+INSERT INTO Pizza (nimi, hinta) VALUES ('Kinkku', '7.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Salami', '7.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Tonnikala', '7.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Bolognese', '7.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Americano', '7.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Francescana', '7.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Hawaiji', '8.00');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Vegetariana', '8.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Superpizza', '8.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Frutti Di Mare', '8.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Kanapizza', '8.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Quattro', '8.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Julia', '8.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Madonna', '8.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Kebabpizza', '8.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Suomi Special', '8.50');
+INSERT INTO Pizza (nimi, hinta) VALUES ('Torino', '8.50');
+
+INSERT INTO PizzaJaLisukkeet (pizzanro, lisukenro) VALUES ('1', '1');
+
+INSERT INTO Kayttaja (nimi, osoite, puhelinNro, kayttajaTunnus, salasana) VALUES ('Matti Meikäläinen', 'Valeosoite 6', '+358400500400', 'Matti666', 'qwerty');
+INSERT INTO Kayttaja (nimi, osoite, puhelinNro, kayttajaTunnus, salasana) VALUES ('Pate Paksukainen', 'Valeosoite 5 C 62', '+358400555400', 'PatTheMat', 'salasana');
+
+INSERT INTO Tilaus (tilausAika, tilauksenTila) VALUES (Now(), '0');
+INSERT INTO Tilaus (tilausAika, tilauksenTila) VALUES (Now(), '1');
+INSERT INTO Tilaus (tilausAika, tilauksenTila) VALUES (Now(), '2');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,46 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Lisuke(
+    lisukenro SERIAL PRIMARY KEY,
+    nimi varchar(50) NOT NULL
+);
+
+CREATE TABLE Pizza(
+    pizzanro SERIAL PRIMARY KEY,
+    nimi varchar(50) NOT NULL,
+    hinta INTEGER NOT NULL
+);
+
+CREATE TABLE PizzaJaLisukkeet(
+    pizzanro INTEGER REFERENCES Pizza(pizzanro),
+    lisukenro INTEGER REFERENCES Lisuke(lisukenro)
+);
+
+CREATE TABLE TarjousHinnat(
+    tarjouksenAlku DATE NOT NULL,
+    tarjouksenLoppu DATE NOT NULL,
+    tarjousProsentti INTEGER NOT NULL
+);
+
+CREATE TABLE Kayttaja(
+    kayttajaId SERIAL PRIMARY KEY,
+    nimi varchar(50) NOT NULL,
+    osoite varchar(50) NOT NULL,
+    puhelinNro varchar(50) NOT NULL
+    kayttajaTunnus varchar(50) NOT NULL,
+    salasana varchar(50) NOT NULL
+);
+
+CREATE TABLE Tilaus(
+    tilausnro SERIAL PRIMARY KEY,
+    tilausAika DATE NOT NULL,
+    tilaaja INTEGER REFERENCES Kayttaja(kayttajaId)
+    tilauksenTila INTEGER NOT NULL
+);
+
+CREATE TABLE TilauksenPizzat(
+    tilausnro INTEGER REFERENCES Tilaus(tilausnro),
+    pizzanro INTEEGR REFERENCES Pizza(pizzanro)
+);
+
+
+

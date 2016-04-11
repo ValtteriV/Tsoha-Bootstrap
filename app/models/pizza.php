22a47b916e6567090483fec1cfe3d14916958cbc
ValtteriV <valtteri.vistiaho@gmail.com>
2016-04-11 23:01:22 +0300

    vko 3 kommit

--- app/models/pizza.php
+++ app/models/pizza.php
@@ -45,10 +45,6 @@
             $row = $query->fetch();
             $this->pizzanro = $row['pizzanro'];
         }
+        
     }
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 

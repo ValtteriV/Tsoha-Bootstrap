b961f53cc68db3332a6c65260c4e76169c12f98b
ValtteriV <valtteri.vistiaho@gmail.com>
2016-05-07 13:35:44 +0300

    asdf

--- lib/base_controller.php
+++ lib/base_controller.php
@@ -6,6 +6,8 @@
       // Toteuta kirjautuneen käyttäjän haku tähän
       if(isset($_SESSION['user'])){
           $user = User::find($_SESSION['user']);
+          
+          return $user;
       }
       return null;
     }
@@ -13,6 +15,13 @@
     public static function check_logged_in(){
       // Toteuta kirjautumisen tarkistus tähän.
       // Jos käyttäjä ei ole kirjautunut sisään, ohjaa hänet toiselle sivulle (esim. kirjautumissivulle).
+        if (isset($_SESSION['user'])){
+            $user = User::find($_SESSION['user']);
+            
+            return $user;    
+        
+        }
+        return null;
     }
 
   }
